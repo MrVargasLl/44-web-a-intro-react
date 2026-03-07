8ff4b9bca205e813fa2a92862d8f23760c374e78
Alvaro Enrique Vargas Llanos <alvaro.vargas.mail@gmail.com>
2026-03-07 18:58:10 -0400

    01-intro-react - Proyecto

--- 01-intro-react/src/App.jsx
+++ 01-intro-react/src/App.jsx
@@ -1,18 +1,29 @@
-import { Footer } from "./components/Footer"
+/* import { Footer } from "./components/Footer"
 import { Header } from "./components/Header"
-import { UserCard } from "./components/UserCard"
+import { UserCard } from "./components/UserCard" */
+import {Tarjeta} from './components/Tarjeta';
 
 //App componente padre e hijo es USerCard
 
-
+/* 
 export const App = () => {
   return (
     <>
     <Header />
-    <UserCard name="Agnes" state="Alive" location="Panama"/>
+{/*     <UserCard name="Agnes" state="Alive" location="Panama"/>
     <UserCard name="Alice" state="Dead" location="Peru"/>
     <UserCard name="Pedro" state="Alive" location="Colombia"/>
-    <Footer />
+    <Footer /> *//* }
     </>
   )
+} */
+export function App() {
+  return (
+    <div>
+      <h1>Tarjeta de Presentación</h1>
+      {/* Renderizamos el componente Tarjeta */}
+      <Tarjeta />
+    </div>
+  );
 }
+export default App;
